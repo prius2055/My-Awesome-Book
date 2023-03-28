062cf6e7bbbdd5d4e10c89f173a2316346a32db5
prius2055 <prius2055@gmail.com>
2023-03-28 12:56:13 +0100

    Fix linters error

--- index.js
+++ index.js
@@ -1,42 +1,41 @@
-import Book from "./modules/Library.js";
-import CurrentDateTime from "./modules/Date.js";
+import Book from './modules/Library.js';
+import CurrentDateTime from './modules/Date.js';
 
-document.addEventListener("DOMContentLoaded", () => {
-  document.querySelector(".date").textContent = CurrentDateTime;
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelector('.date').textContent = CurrentDateTime;
   const book = new Book();
   book.init();
 });
 
-const listLink = document.querySelector(".list");
-const addNewLink = document.querySelector(".add-new");
-const contactLink = document.querySelector(".contact");
+const listLink = document.querySelector('.list');
+const addNewLink = document.querySelector('.add-new');
+const contactLink = document.querySelector('.contact');
 
-const allBooksSection = document.querySelector(".all-books");
-const addBookSection = document.querySelector(".add-book");
-const contactSection = document.querySelector(".contact-form");
+const allBooksSection = document.querySelector('.all-books');
+const addBookSection = document.querySelector('.add-book');
+const contactSection = document.querySelector('.contact-form');
 
-allBooksSection.style.display = "block";
-addBookSection.style.display = "none";
-contactSection.style.display = "none";
+allBooksSection.style.display = 'block';
+addBookSection.style.display = 'none';
+contactSection.style.display = 'none';
 
-listLink.addEventListener("click", (e) => {
+listLink.addEventListener('click', (e) => {
   e.preventDefault();
-  allBooksSection.style.display = "block";
-  addBookSection.style.display = "none";
-  contactSection.style.display = "none";
+  allBooksSection.style.display = 'block';
+  addBookSection.style.display = 'none';
+  contactSection.style.display = 'none';
 });
 
-addNewLink.addEventListener("click", (e) => {
+addNewLink.addEventListener('click', (e) => {
   e.preventDefault();
-  addBookSection.style.display = "block";
-  allBooksSection.style.display = "none";
-  contactSection.style.display = "none";
+  addBookSection.style.display = 'block';
+  allBooksSection.style.display = 'none';
+  contactSection.style.display = 'none';
 });
 
-contactLink.addEventListener("click", (e) => {
+contactLink.addEventListener('click', (e) => {
   e.preventDefault();
-  contactSection.style.display = "block";
-  addBookSection.style.display = "none";
-  allBooksSection.style.display = "none";
+  contactSection.style.display = 'block';
+  addBookSection.style.display = 'none';
+  allBooksSection.style.display = 'none';
 });
-
--- modules/Date.js
+++ modules/Date.js
@@ -1,4 +1,4 @@
-import { DateTime } from "../node_modules/luxon/build/es6/luxon.js";
+import { DateTime } from '../node_modules/luxon/build/es6/luxon.js';
 
 const CurrentDateTime = DateTime.local().toLocaleString(DateTime.DATETIME_MED);
 
