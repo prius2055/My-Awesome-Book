3c517fb32f9ca05b43394c4141600d00e53bb9e8
prius2055 <prius2055@gmail.com>
2023-03-28 12:53:10 +0100

    Create the Date.js module and export the const CurrentDateTime.Import the CurrentDateTime and use it in index.js. Include double quotes on the book title.

--- index.js
+++ index.js
@@ -1,44 +1,42 @@
-import Book from './modules/Library.js';
+import Book from "./modules/Library.js";
+import CurrentDateTime from "./modules/Date.js";
 
-import { DateTime } from './node_modules/luxon/build/es6/luxon.js';
-
-const now = DateTime.local().toLocaleString(DateTime.DATETIME_MED);
-
-document.addEventListener('DOMContentLoaded', () => {
-  document.querySelector('.date').textContent = now;
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelector(".date").textContent = CurrentDateTime;
   const book = new Book();
   book.init();
 });
 
-const listLink = document.querySelector('.list');
-const addNewLink = document.querySelector('.add-new');
-const contactLink = document.querySelector('.contact');
+const listLink = document.querySelector(".list");
+const addNewLink = document.querySelector(".add-new");
+const contactLink = document.querySelector(".contact");
 
-const allBooksSection = document.querySelector('.all-books');
-const addBookSection = document.querySelector('.add-book');
-const contactSection = document.querySelector('.contact-form');
+const allBooksSection = document.querySelector(".all-books");
+const addBookSection = document.querySelector(".add-book");
+const contactSection = document.querySelector(".contact-form");
 
-allBooksSection.style.display = 'block';
-addBookSection.style.display = 'none';
-contactSection.style.display = 'none';
+allBooksSection.style.display = "block";
+addBookSection.style.display = "none";
+contactSection.style.display = "none";
 
-listLink.addEventListener('click', (e) => {
+listLink.addEventListener("click", (e) => {
   e.preventDefault();
-  allBooksSection.style.display = 'block';
-  addBookSection.style.display = 'none';
-  contactSection.style.display = 'none';
+  allBooksSection.style.display = "block";
+  addBookSection.style.display = "none";
+  contactSection.style.display = "none";
 });
 
-addNewLink.addEventListener('click', (e) => {
+addNewLink.addEventListener("click", (e) => {
   e.preventDefault();
-  addBookSection.style.display = 'block';
-  allBooksSection.style.display = 'none';
-  contactSection.style.display = 'none';
+  addBookSection.style.display = "block";
+  allBooksSection.style.display = "none";
+  contactSection.style.display = "none";
 });
 
-contactLink.addEventListener('click', (e) => {
+contactLink.addEventListener("click", (e) => {
   e.preventDefault();
-  contactSection.style.display = 'block';
-  addBookSection.style.display = 'none';
-  allBooksSection.style.display = 'none';
+  contactSection.style.display = "block";
+  addBookSection.style.display = "none";
+  allBooksSection.style.display = "none";
 });
+
--- modules/Library.js
+++ modules/Library.js
@@ -2,38 +2,38 @@ export default class Book {
   constructor() {
     this.allBooks = [];
 
-    if (localStorage.getItem('allBooks') !== null) {
-      this.allBooks = JSON.parse(localStorage.getItem('allBooks'));
+    if (localStorage.getItem("allBooks") !== null) {
+      this.allBooks = JSON.parse(localStorage.getItem("allBooks"));
     }
   }
 
   addBook(title, author) {
     const book = { title, author };
     this.allBooks.push(book);
-    localStorage.setItem('allBooks', JSON.stringify(this.allBooks));
+    localStorage.setItem("allBooks", JSON.stringify(this.allBooks));
     this.displayBooks();
   }
 
   removeBook(index) {
     this.allBooks.splice(index, 1);
-    localStorage.setItem('allBooks', JSON.stringify(this.allBooks));
+    localStorage.setItem("allBooks", JSON.stringify(this.allBooks));
     this.displayBooks();
   }
 
   displayBooks() {
-    const listB = document.getElementById('main-list');
-    listB.innerHTML = '';
+    const listB = document.getElementById("main-list");
+    listB.innerHTML = "";
 
     this.allBooks.reverse().forEach((b, index) => {
-      const div = document.createElement('div');
-      div.className = 'book-list';
-      const bookTitle = document.createElement('p');
-      bookTitle.textContent = `${b.title} by ${b.author}`;
+      const div = document.createElement("div");
+      div.className = "book-list";
+      const bookTitle = document.createElement("p");
+      bookTitle.textContent = `"${b.title}" by ${b.author}`;
       div.appendChild(bookTitle);
-      const removeBtn = document.createElement('button');
-      removeBtn.type = 'button';
-      removeBtn.textContent = 'Remove';
-      removeBtn.addEventListener('click', () => {
+      const removeBtn = document.createElement("button");
+      removeBtn.type = "button";
+      removeBtn.textContent = "Remove";
+      removeBtn.addEventListener("click", () => {
         this.removeBook(index);
       });
       div.appendChild(removeBtn);
@@ -43,13 +43,13 @@ export default class Book {
   }
 
   initializeForm() {
-    const myForm = document.getElementById('book-form');
+    const myForm = document.getElementById("book-form");
 
-    myForm.addEventListener('submit', (e) => {
+    myForm.addEventListener("submit", (e) => {
       e.preventDefault();
       const title = myForm.title.value;
       const author = myForm.author.value;
-      if (title !== '' && author !== '') {
+      if (title !== "" && author !== "") {
         this.addBook(title, author);
         myForm.reset();
       }
@@ -62,3 +62,4 @@ export default class Book {
     this.displayBooks();
   }
 }
+
